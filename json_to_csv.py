46d8b81c5bdb73f186b9904b7d6a058fbb0bbf71
16699a0202 <61870342+16699a0202@users.noreply.github.com>
2021-04-05 06:19:13 +0530

    Add files via upload

---
+++ json_to_csv.py
@@ -0,0 +1,48 @@
+import argparse
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+parser=argparse.ArgumentParser()
+parser.add_argument(
+    '--input',
+    dest='input',
+    required=True,
+    default=''
+)
+
+parser.add_argument(
+    '--output',
+    dest='output',
+    required=True,
+    default='output_data.txt'
+)
+
+known_args,pipeline_args=parser.parse_known_args()
+
+p1=beam.Pipeline(options=PipelineOptions(pipeline_args))
+
+def split_row(row):
+    l=row.split(',') #['{"id":1', '"first_name":"Ynes"', '"last_name":"MacCarter"', '"email":"ymaccarter0@wikia.com"', '"gender":"Genderfluid"}']
+    d={}
+    for i in l:
+        s=i.split(":") #[{"id",'1']
+        d[s[0]]=s[1]
+    return list(d.values()) #['1', '"Ynes"', '"MacCarter"', '"ymaccarter0@wikia.com"', '"Genderfluid"}']
+
+def join_row(row):
+    row= ','.join(row) #1,"Ynes","MacCarter","ymaccarter0@wikia.com","Genderfluid"}
+    return row[:-1] # 1,"Ynes","MacCarter","ymaccarter0@wikia.com","Genderfluid"
+
+doctorcount=(
+    p1
+    | 'Read from a file' >> beam.io.ReadFromText(known_args.input)#{"id":1,"first_name":"Ynes","last_name":"MacCarter","email":"ymaccarter0@wikia.com","gender":"Genderfluid"}
+    #| beam.Map(split_row)
+    #| beam.Map(join_row)
+    | 'write to a file' >>beam.io.Write(beam.io.BigQuerySink(known_args.output,schema='id:INTEGER,first_name:STRING,last_name:STRING,email:STRING,gender:STRING',
+                                                             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+                                                             write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE))
+)
+
+p1.run().wait_until_finish()
+
+#python -m json_to_csv.py --input "MOCK_DATA (1).json" --output "output_data.txt"
